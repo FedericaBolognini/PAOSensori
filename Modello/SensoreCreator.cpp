35465c604c240b73c33075b24d78b843c62c3350
MatteoCus <matteo.cusin58@gmail.com>
2024-07-26 17:20:39 +0200

    Fix sensorecreator

--- Modello/SensoreCreator.cpp
+++ Modello/SensoreCreator.cpp
@@ -1,16 +1,16 @@
 #include "sensorecreator.h"
 
 
-static Sensore *createSensore(const std::string &Type, unsigned int ID, const std::string &Nome, unsigned int Precisione, double MinValidTemperatura, double MaxValidTemperatura, 
+Sensore *SensoreCreator::createSensore(const std::string &Type, unsigned int ID, const std::string &Nome, unsigned int Precisione, double MinValidTemperatura, double MaxValidTemperatura, 
         int MinValidAlcalinità, int MaxValidAlcalinità, int MinValidAcidità, int MaxValidAcidità, 
         int MinValidOssigeno, int MaxValidOssigeno, int MinValidCO2, int MaxValidCO2,
         int MinValidUmidità, int MaxValidUmidità, int MinValidPH, int MaxValidPH){
-            if (type == "SensoreAcqua")
+            if (Type == "SensoreAcqua")
                 return new SensoreAcqua(Nome, Precisione, ID, MinValidTemperatura, MaxValidTemperatura, MinValidAlcalinità, MaxValidAlcalinità, MinValidAcidità, MaxValidAcidità);
-            else if (type == "SensoreAria")
+            else if (Type == "SensoreAria")
                 return new SensoreAria(Nome, Precisione, ID, MinValidTemperatura, MaxValidTemperatura, MinValidCO2, MaxValidCO2, MinValidOssigeno, MaxValidOssigeno);
-            else if (type == "SensoreSuolo")
-                return new SensoreSuolo(Nome, Precisione, ID, MinValidTemperatura, MaxValidTemperatura, MinValidUmidità, MaxValidUmidità, MinValidPH, MaxValidPH);
+            else if (Type == "SensoreSuolo")
+                return new SensoreSuolo(Nome, Precisione, ID, MinValidTemperatura, MaxValidTemperatura, MinValidPH, MaxValidPH, MinValidUmidità, MaxValidUmidità);
             else {
                 throw std::invalid_argument("Tipo di sensore non valido");
             }
--- Modello/SensoreCreator.h
+++ Modello/SensoreCreator.h
@@ -2,11 +2,14 @@
 #define SENSORECREATOR_H
 
 #include "Sensore.h"
+#include "SensoreAcqua.h"
+#include "SensoreAria.h"
+#include "SensoreSuolo.h"
 #include <stdexcept>
 #include <string>
 #include <vector>
 
-class createSensore {
+class SensoreCreator {
 public:
 
     /**
@@ -29,7 +32,7 @@ public:
      * @param MinValidPH:               valore minimo dell'intervallo valido di PH "ammissibile"
      * @param MaxValidPH:               valore massimo dell'intervallo valido di PH "ammissibile"
      * @return Sensore *:               sensore creato sulla base dei parametri in ingresso
-     * @throw std::invalid_argument     se il nome è vuoto - se la precisione supera il 100% - se il range di valori per la temperatura, alcalinità, acidità, ossigeno, co2, umidità non è corretto(basta una delle seguenti condizioni)-
+     * @throw std::invalid_argument     se il nome è vuoto - se la precisione supera il 100% - se il range di valori per la temperatura, alcalinità, acidità, ossigeno, co2, umidità non è corretto(basta una delle seguenti condizioni) - se il tipo non è valido
      */
 
     static Sensore *createSensore(const std::string &Type, unsigned int ID, const std::string &Nome, unsigned int Precisione, double MinValidTemperatura, double MaxValidTemperatura, 
